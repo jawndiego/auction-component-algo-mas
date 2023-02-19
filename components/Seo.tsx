f689aebc2be1a0554eabc2221744f6d1b9519b7c
jawn <62079777+jawndiego@users.noreply.github.com>
2023-02-19 13:15:20 -0500

    UPDATE SEO
    IMAGES

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -22,13 +22,13 @@ export function Seo({ title, description, url, ogImage }: SeoProps) {
       <meta name="description" content={description || defaultDescription} />
       <link rel="icon" type="image/png" sizes="24x24" href={favicon} />
       <meta property="og:url" content={url || defaultOGURL} />
-      <meta property="og:title" content={title || ''} />
-      <meta property="og:description" content={description || defaultDescription} />
+      <meta property="og:title" content={title || 'JDR, AUCTION'} />
+      <meta property="og:description" content="A PLACE TO COP JDR 1 OF 1s"/>
       <meta name="twitter:creator" content={`@${process.env.NEXT_PUBLIC_TWITTER_HANDLE}`} />
       <meta name="twitter:site" content={url || defaultOGURL} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content={ogImage || defaultOGImage} />
-      <meta property="og:image" content={ogImage || defaultOGImage} />
+      <meta name="twitter:image" content="https://i.seadn.io/gcs/files/5729f7790694c292d143ed5ff1cb4530.png?auto=format&w=1000" />
+      <meta property="og:image" content="https://i.seadn.io/gcs/files/5729f7790694c292d143ed5ff1cb4530.png?auto=format&w=1000" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
     </NextHead>
